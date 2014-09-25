36fbe7385cccf3c57d31256d9100a9ebc5a08433
Steve McFarlin <steve@tokbox.com>
2014-09-24 20:41:21 -0400

    ADD External Audio Device sample application
    This application demonstrates how a developer might use the Audio Driver API
    in version 2.3.0 of the OpenTok iOS SDK. It operates just as the Hello World
    exmaple does, with an additional call to set the Audio Driver and an
    implementation file for the Audio Driver itself.

---
+++ 7.External-Audio-Device/External-Audio-Device/ViewController.h
@@ -0,0 +1,12 @@
+//
+//  ViewController.h
+//  External-Audio-Device
+//
+//  Copyright (c) 2014 TokBox Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ 7.External-Audio-Device/External-Audio-DeviceTests/External_Audio_DeviceTests.m
@@ -0,0 +1,34 @@
+//
+//  External_Audio_DeviceTests.m
+//  External-Audio-DeviceTests
+//
+//  Created by Steve McFarlin on 7/7/14.
+//  Copyright (c) 2014 TokBox Inc. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface External_Audio_DeviceTests : XCTestCase
+
+@end
+
+@implementation External_Audio_DeviceTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
