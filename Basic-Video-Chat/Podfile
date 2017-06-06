b320be522c21956082ec584ce36b8980a28c8a08
Lucas Huang <xhuang0802@gmail.com>
2017-06-06 16:01:16 -0700

    restructure to populate Podfile to each folder (#155) (#157)
    * restructure to populate Podfile to each folder
    
    * remove a Podfile; throw failure when it fails
    
    * fix a file reference error
    
    * fix another file reference errors
    
    * use 2.11.1
    
    * add a convenient place to replace version at root dir

---
+++ Basic-Video-Chat/Podfile
@@ -0,0 +1,8 @@
+platform :ios, '9.0'
+use_frameworks!
+
+require_relative '../OpenTokSDKVersion'
+
+target 'Basic-Video-Chat' do
+  pod 'OpenTok', OpenTokSDKVersion
+end
--- travis_build.sh
+++ travis_build.sh
@@ -1,19 +1,47 @@
 #!/bin/sh
 
-xcodebuild -workspace Opentok-iOS-samples.xcworkspace  -list 
-xcodebuild -workspace Opentok-iOS-samples.xcworkspace  -scheme Basic-Video-Chat -sdk iphonesimulator ONLY_ACTIVE_ARCH=NO
-xcodebuild -workspace Opentok-iOS-samples.xcworkspace  -scheme Custom-Video-Driver -sdk iphonesimulator ONLY_ACTIVE_ARCH=NO
-xcodebuild -workspace Opentok-iOS-samples.xcworkspace  -scheme Custom-Audio-Driver -sdk iphonesimulator ONLY_ACTIVE_ARCH=NO
-xcodebuild -workspace Opentok-iOS-samples.xcworkspace  -scheme Screen-Sharing -sdk iphonesimulator ONLY_ACTIVE_ARCH=NO
-xcodebuild -workspace Opentok-iOS-samples.xcworkspace  -scheme Live-Photo-Capture -sdk iphonesimulator ONLY_ACTIVE_ARCH=NO
-xcodebuild -workspace Opentok-iOS-samples.xcworkspace  -scheme Simple-Multiparty -sdk iphonesimulator ONLY_ACTIVE_ARCH=NO
+set -e
 
-xcodebuild -workspace Opentok-iOS-samples.xcworkspace  -scheme Overlay-Graphics -sdk iphonesimulator ONLY_ACTIVE_ARCH=NO
+cd Basic-Video-Chat/
+pod install
+xcodebuild -workspace Basic-Video-Chat.xcworkspace  -scheme Basic-Video-Chat -sdk iphonesimulator ONLY_ACTIVE_ARCH=NO
 
-xcodebuild -workspace Opentok-iOS-samples.xcworkspace  -scheme Audio-Levels -sdk iphonesimulator ONLY_ACTIVE_ARCH=NO
+cd ../Custom-Video-Driver/
+pod install
+xcodebuild -workspace Custom-Video-Driver.xcworkspace  -scheme Custom-Video-Driver -sdk iphonesimulator ONLY_ACTIVE_ARCH=NO
 
-xcodebuild -workspace Opentok-iOS-samples.xcworkspace  -scheme Ringtones -sdk iphonesimulator ONLY_ACTIVE_ARCH=NO
+cd ../Custom-Audio-Driver/
+pod install
+xcodebuild -workspace Custom-Audio-Driver.xcworkspace  -scheme Custom-Audio-Driver -sdk iphonesimulator ONLY_ACTIVE_ARCH=NO
 
-xcodebuild -workspace Opentok-iOS-samples.xcworkspace  -scheme Archiving -sdk iphonesimulator ONLY_ACTIVE_ARCH=NO
+cd ../Screen-Sharing/
+pod install
+xcodebuild -workspace Screen-Sharing.xcworkspace  -scheme Screen-Sharing -sdk iphonesimulator ONLY_ACTIVE_ARCH=NO
 
-xcodebuild -workspace Opentok-iOS-samples.xcworkspace  -scheme Signaling -sdk iphonesimulator ONLY_ACTIVE_ARCH=NO
+cd ../Live-Photo-Capture/
+pod install
+xcodebuild -workspace Live-Photo-Capture.xcworkspace  -scheme Live-Photo-Capture -sdk iphonesimulator ONLY_ACTIVE_ARCH=NO
+
+cd ../Simple-Multiparty/
+pod install
+xcodebuild -workspace Simple-Multiparty.xcworkspace  -scheme Simple-Multiparty -sdk iphonesimulator ONLY_ACTIVE_ARCH=NO
+
+cd ../Overlay-Graphics/
+pod install
+xcodebuild -workspace Overlay-Graphics.xcworkspace  -scheme Overlay-Graphics -sdk iphonesimulator ONLY_ACTIVE_ARCH=NO
+
+cd ../Audio-Levels/
+pod install
+xcodebuild -workspace Audio-Levels.xcworkspace  -scheme Audio-Levels -sdk iphonesimulator ONLY_ACTIVE_ARCH=NO
+
+cd ../Ringtones/
+pod install
+xcodebuild -workspace Ringtones.xcworkspace  -scheme Ringtones -sdk iphonesimulator ONLY_ACTIVE_ARCH=NO
+
+cd ../Archiving/
+pod install
+xcodebuild -workspace Archiving.xcworkspace  -scheme Archiving -sdk iphonesimulator ONLY_ACTIVE_ARCH=NO
+
+cd ../Signaling/
+pod install
+xcodebuild -workspace Signaling.xcworkspace  -scheme Signaling -sdk iphonesimulator ONLY_ACTIVE_ARCH=NO
