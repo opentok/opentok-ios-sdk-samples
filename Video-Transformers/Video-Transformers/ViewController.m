084568d0c0fcb2916d0dbe1ef3c9aaf1fc29dce8
Goncalo Mendes <goncalo.mendes@vonage.com>
2023-06-13 09:42:36 +0200

    Add Video transformers sample

---
+++ Video-Transformers/Video-Transformers/ViewController.m
@@ -0,0 +1,324 @@
+//
+//  ViewController.m
+//  Hello-World
+//
+//  Copyright (c) 2013 TokBox, Inc. All rights reserved.
+//
+
+#import "ViewController.h"
+#import <OpenTok/OpenTok.h>
+
+// *** Fill the following variables using your own Project info  ***
+// ***          https://dashboard.tokbox.com/projects            ***
+// Replace with your OpenTok API key
+static NSString* const kApiKey = @"";
+// Replace with your generated session ID
+static NSString* const kSessionId = @"";
+// Replace with your generated token
+static NSString* const kToken = @"";
+
+@interface ViewController ()<OTSessionDelegate, OTSubscriberDelegate, OTPublisherDelegate>
+@property (nonatomic) OTSession *session;
+@property (nonatomic) OTPublisher *publisher;
+@property (nonatomic) OTSubscriber *subscriber;
+@end
+
+static double widgetHeight = 240;
+static double widgetWidth = 320;
+
+@interface customTransformer : NSObject <OTCustomVideoTransformer>
+
+- (UIImage *)resizeImage:(UIImage *)image toSize:(CGSize)size;
+
+@end
+
+
+@implementation customTransformer
+
+- (UIImage *)resizeImage:(UIImage *)image toSize:(CGSize)size {
+    UIGraphicsBeginImageContextWithOptions(size, NO, 0.0);
+    [image drawInRect:CGRectMake(0, 0, size.width, size.height)];
+    UIImage *resizedImage = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    return resizedImage;
+}
+
+- (void)transform:(nonnull OTVideoFrame *)videoFrame {
+    
+    UIImage* image = [UIImage imageNamed:@"Vonage_Logo.png"];
+
+    uint32_t videoWidth = videoFrame.format.imageWidth;
+    uint32_t videoHeight = videoFrame.format.imageHeight;
+
+    // Calculate the desired size of the image
+    CGFloat desiredWidth = videoWidth / 8;  // Adjust this value as needed
+    CGFloat desiredHeight = image.size.height * (desiredWidth / image.size.width);
+
+    // Resize the image to the desired size
+    UIImage *resizedImage = [self resizeImage:image toSize:CGSizeMake(desiredWidth, desiredHeight)];
+
+    // Get pointer to the Y plane
+    uint8_t* yPlane = [videoFrame getPlaneBinaryData:0];
+    
+    // Create a CGContext from the Y plane
+    CGContextRef context = CGBitmapContextCreate(yPlane, videoWidth, videoHeight, 8, videoWidth, CGColorSpaceCreateDeviceGray(), kCGImageAlphaNone);
+    
+    // Location of the image (in this case right bottom corner)
+    CGFloat x = videoWidth * 4/5;
+    CGFloat y = videoHeight * 1/5;
+    
+    // Draw the resized image on top of the Y plane
+    CGRect rect = CGRectMake(x, y, desiredWidth, desiredHeight);
+    CGContextDrawImage(context, rect, resizedImage.CGImage);
+    
+    CGContextRelease(context);
+}
+
+@end
+
+customTransformer* logoTransformer;
+
+@implementation ViewController
+
+#pragma mark - View lifecycle
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+    // Step 1: As the view comes into the foreground, initialize a new instance
+    // of OTSession and begin the connection process.
+    _session = [[OTSession alloc] initWithApiKey:kApiKey
+                                       sessionId:kSessionId
+                                        delegate:self];
+    [self doConnect];
+}
+
+- (BOOL)prefersStatusBarHidden
+{
+    return YES;
+}
+
+- (BOOL)shouldAutorotate {
+    return UIUserInterfaceIdiomPhone != [[UIDevice currentDevice] userInterfaceIdiom];
+}
+#pragma mark - OpenTok methods
+
+/** 
+ * Asynchronously begins the session connect process. Some time later, we will
+ * expect a delegate method to call us back with the results of this action.
+ */
+- (void)doConnect
+{
+    OTError *error = nil;
+    
+    [_session connectWithToken:kToken error:&error];
+    if (error)
+    {
+        [self showAlert:[error localizedDescription]];
+    }
+}
+
+/**
+ * Sets up an instance of OTPublisher to use with this session. OTPubilsher
+ * binds to the device camera and microphone, and will provide A/V streams
+ * to the OpenTok session.
+ */
+- (void)doPublish
+{
+    OTPublisherSettings *settings = [[OTPublisherSettings alloc] init];
+    settings.name = [UIDevice currentDevice].name;
+    _publisher = [[OTPublisher alloc] initWithDelegate:self settings:settings];
+   
+    OTError *error = nil;
+    [_session publish:_publisher error:&error];
+    if (error)
+    {
+        [self showAlert:[error localizedDescription]];
+    }
+    
+    [self.view addSubview:_publisher.view];
+    [_publisher.view setFrame:CGRectMake(0, 0, widgetWidth, widgetHeight)];
+    
+    // Create background blur Vonage transformer
+    OTVideoTransformer *BackgroundBlur = [[OTVideoTransformer alloc] initWithName:@"BackgroundBlur" properties:@"{\"radius\":\"High\"}"];
+        
+    // Create custom transformer
+    logoTransformer = [customTransformer alloc];
+    OTVideoTransformer *myCustomTransformer = [[OTVideoTransformer alloc] initWithName:@"logo" transformer:logoTransformer];
+
+    NSMutableArray * myVideoTransformers = [[NSMutableArray alloc] init];
+
+    [myVideoTransformers addObject:BackgroundBlur];
+    [myVideoTransformers addObject:myCustomTransformer];
+
+    // Set video transformers to publisher video stream
+    _publisher.videoTransformers = [[NSMutableArray alloc] initWithArray:myVideoTransformers];
+    
+    // Remove transformers from publisher stream
+    //_publisher.videoTransformers = [[NSMutableArray alloc] init];
+}
+
+/**
+ * Cleans up the publisher and its view. At this point, the publisher should not
+ * be attached to the session any more.
+ */
+- (void)cleanupPublisher {
+    [_publisher.view removeFromSuperview];
+    _publisher = nil;
+    // this is a good place to notify the end-user that publishing has stopped.
+}
+
+/**
+ * Instantiates a subscriber for the given stream and asynchronously begins the
+ * process to begin receiving A/V content for this stream. Unlike doPublish, 
+ * this method does not add the subscriber to the view hierarchy. Instead, we 
+ * add the subscriber only after it has connected and begins receiving data.
+ */
+- (void)doSubscribe:(OTStream*)stream
+{
+    _subscriber = [[OTSubscriber alloc] initWithStream:stream delegate:self];
+    
+    OTError *error = nil;
+    [_session subscribe:_subscriber error:&error];
+    if (error)
+    {
+        [self showAlert:[error localizedDescription]];
+    }
+}
+
+/**
+ * Cleans the subscriber from the view hierarchy, if any.
+ * NB: You do *not* have to call unsubscribe in your controller in response to
+ * a streamDestroyed event. Any subscribers (or the publisher) for a stream will
+ * be automatically removed from the session during cleanup of the stream.
+ */
+- (void)cleanupSubscriber
+{
+    [_subscriber.view removeFromSuperview];
+    _subscriber = nil;
+}
+
+# pragma mark - OTSession delegate callbacks
+
+- (void)sessionDidConnect:(OTSession*)session
+{
+    NSLog(@"sessionDidConnect (%@)", session.sessionId);
+    
+    // Step 2: We have successfully connected, now instantiate a publisher and
+    // begin pushing A/V streams into OpenTok.
+    [self doPublish];
+}
+
+- (void)sessionDidDisconnect:(OTSession*)session
+{
+    NSString* alertMessage =
+    [NSString stringWithFormat:@"Session disconnected: (%@)",
+     session.sessionId];
+    NSLog(@"sessionDidDisconnect (%@)", alertMessage);
+}
+
+
+- (void)session:(OTSession*)mySession
+  streamCreated:(OTStream *)stream
+{
+    NSLog(@"session streamCreated (%@)", stream.streamId);
+    
+    if (nil == _subscriber)
+    {
+        [self doSubscribe:stream];
+    }
+}
+
+- (void)session:(OTSession*)session
+streamDestroyed:(OTStream *)stream
+{
+    NSLog(@"session streamDestroyed (%@)", stream.streamId);
+    
+    if ([_subscriber.stream.streamId isEqualToString:stream.streamId])
+    {
+        [self cleanupSubscriber];
+    }
+}
+
+- (void)  session:(OTSession *)session
+connectionCreated:(OTConnection *)connection
+{
+    NSLog(@"session connectionCreated (%@)", connection.connectionId);
+}
+
+- (void)    session:(OTSession *)session
+connectionDestroyed:(OTConnection *)connection
+{
+    NSLog(@"session connectionDestroyed (%@)", connection.connectionId);
+    if ([_subscriber.stream.connection.connectionId
+         isEqualToString:connection.connectionId])
+    {
+        [self cleanupSubscriber];
+    }
+}
+
+- (void) session:(OTSession*)session
+didFailWithError:(OTError*)error
+{
+    NSLog(@"didFailWithError: (%@)", error);
+}
+
+# pragma mark - OTSubscriber delegate callbacks
+
+- (void)subscriberDidConnectToStream:(OTSubscriberKit*)subscriber
+{
+    NSLog(@"subscriberDidConnectToStream (%@)",
+          subscriber.stream.connection.connectionId);
+    assert(_subscriber == subscriber);
+    [_subscriber.view setFrame:CGRectMake(0, widgetHeight, widgetWidth,
+                                         widgetHeight)];
+    [self.view addSubview:_subscriber.view];
+}
+
+- (void)subscriber:(OTSubscriberKit*)subscriber
+  didFailWithError:(OTError*)error
+{
+    NSLog(@"subscriber %@ didFailWithError %@",
+          subscriber.stream.streamId,
+          error);
+}
+
+# pragma mark - OTPublisher delegate callbacks
+
+- (void)publisher:(OTPublisherKit *)publisher
+    streamCreated:(OTStream *)stream
+{
+    NSLog(@"Publishing");
+}
+
+- (void)publisher:(OTPublisherKit*)publisher
+  streamDestroyed:(OTStream *)stream
+{
+    if ([_subscriber.stream.streamId isEqualToString:stream.streamId])
+    {
+        [self cleanupSubscriber];
+    }
+    
+    [self cleanupPublisher];
+}
+
+- (void)publisher:(OTPublisherKit*)publisher
+ didFailWithError:(OTError*) error
+{
+    NSLog(@"publisher didFailWithError %@", error);
+    [self cleanupPublisher];
+}
+
+- (void)showAlert:(NSString *)string
+{
+    // show alertview on main UI
+	dispatch_async(dispatch_get_main_queue(), ^{
+        UIAlertController *alertVC = [UIAlertController alertControllerWithTitle:@"OTError"
+                                                                         message:string
+                                                                  preferredStyle:UIAlertControllerStyleAlert];
+        [self presentViewController:alertVC animated:YES completion:nil];
+    });
+}
+
+@end
